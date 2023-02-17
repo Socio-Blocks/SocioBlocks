c42d25babc4436fc5f572826a51d97f50e8e5a03
Madan301 <111639304+Madan301@users.noreply.github.com>
2023-02-17 16:39:55 +0530

    updated fence script

--- fence.py
+++ fence.py
@@ -2,34 +2,33 @@ import requests
 import json
 import random
 headers = {
-    'Authorization': 'prj_live_sk_...',
+    'Authorization': 'test-server api',
     'Content-Type': 'application/x-www-form-urlencoded',
 }
 radius = 2
 
 
-def geofence_creator(tag,tag_id,longitude,latitude):
-    data = 'description=pothole&type=circle&coordinates=[{},{}]&radius={}'%longitude,latitude,radius
-    response = requests.put('https://api.radar.io/v1/geofences/'+tag+'/'+tag_id, headers=headers, data=data)
-
-
-def geofence_checker(longitude,latitude):
-    url = 'https://api.radar.io/v1/context?coordinates={},{}'%latitude,longitude
+def geofence_checker(latitude,longitude):
+    url = 'https://api.radar.io/v1/context?coordinates=%(latitude)s,%(longitude)s'%{"latitude":latitude,"longitude":longitude}
     response = requests.get(url, headers=headers)
     resp = response.content
     resp_data = json.loads(resp.decode("UTF-8"))
-    geofence = resp_data['context']['geofences']['tag']
+    geofence = resp_data['context']['geofences']
     if geofence:
-        print("ur inside the geofence nigga.. no coins fr you")
+        print(geofence)
+        print("CAll checker function of smart contract to check if the pithole geofence is older tah 10 days")
     else:
-        print("lucky fella u get some coins now")
-        tag = resp_data['context']['place']['postalCode']['code']
-        tag_id = random.randint(100,999)
-        geofence_creator(tag,tag_id,longitude,latitude)
+        print("no fences found")
+        tag_id = random.randint(100, 999)
+        tag = str(tag_id)
+        geofence_creator(tag,longitude,latitude)
+        print("Call add_reporter function of the smart contract give params of wallet_address,..etc")
 
 
+def geofence_creator(tag_id,longitude,latitude):
+    data = 'description=pothole&type=circle&coordinates=[%(longitude)s,%(latitude)s]&radius=2'%{'longitude' : longitude, 'latitude':latitude}
+    response = requests.put('https://api.radar.io/v1/geofences/potholeplace/'+tag_id, headers=headers, data=data)
+    print(response)
 
-
-
-
-
+#uncomment to run test function
+# geofence_checker('13.012083622851279', '77.58395057632069')
