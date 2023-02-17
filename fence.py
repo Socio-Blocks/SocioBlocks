0d49d0feffee835e651d54be5c181728dec8ed7a
Madan301 <111639304+Madan301@users.noreply.github.com>
2023-02-18 01:01:00 +0530

    Update fence.py

--- fence.py
+++ fence.py
@@ -2,10 +2,10 @@ import requests
 import json
 import random
 headers = {
-    'Authorization': 'test-server api',
+    'Authorization': '-api-key',
     'Content-Type': 'application/x-www-form-urlencoded',
 }
-radius = 2.5
+radius = 2
 
 
 def geofence_checker(latitude,longitude):
@@ -15,8 +15,9 @@ def geofence_checker(latitude,longitude):
     resp_data = json.loads(resp.decode("UTF-8"))
     geofence = resp_data['context']['geofences']
     if geofence:
-        print(geofence)
-        print("CAll checker function of smart contract to check if the pithole geofence is older tah 10 days")
+        geofence_id = resp_data['context']['geofences'][0]['externalId']
+        print(geofence_id)
+        print("CAll checker function of smart contract to check if the pothole geofence is older than 10 days")
     else:
         print("no fences found")
         tag_id = random.randint(100, 999)
@@ -31,4 +32,4 @@ def geofence_creator(tag_id,longitude,latitude):
     print(response)
 
 #uncomment to run test function
-# geofence_checker('13.012083622851279', '77.58395057632069')
+#geofence_checker('13.007834348790443', '77.57282200643067')
