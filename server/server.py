c06379101d89a6f786dc0008b65316929e2b509d
Madan301 <111639304+Madan301@users.noreply.github.com>
2023-02-19 03:41:06 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -10,10 +10,13 @@ from io import BytesIO
 import cv2
 from pinatapy import PinataPy
 import requests
-
+import json
 
 pinata_api_key = "eac222c6682ef06e91f8"
 pinata_secret_api_key = "09e5e8d1c6ff279ce8ccb581a440bbe6764ec18dc7ed7302a5bce666e62b9ad6"
+headers = {
+    'x-kek-auth': '5Uo9hDVIhwRmDRdz.G1w5Wg8vfQIlVu6J6,he9tylVMOj8hIRja5b',
+}
 
 x = datetime.datetime.now()
 
@@ -21,17 +24,28 @@ x = datetime.datetime.now()
 app = Flask(__name__)
 CORS(app)
 
+
 def uploader(filename):
     pinata = PinataPy(pinata_api_key, pinata_secret_api_key)
-    response = pinata.pin_file_to_ipfs(filename+'.jpg')
+    response = pinata.pin_file_to_ipfs(filename)
     return response['IpfsHash']
 
 
 def retriever(hashh,filename):
     url = "https://ipfs.io/ipfs/"+hashh
     img_data = requests.get(url).content
-    with open(filename+'.jpg', 'wb') as writer:
+    with open(filename, 'wb') as writer:
         writer.write(img_data)
+    files = {
+        'file': open(filename, 'rb'),
+    }
+    response = requests.post('https://kek.sh/api/v1/posts', headers=headers, files=files)
+    a = response.content
+    b = json.loads(a)
+    c = b['filename']
+    img_url = 'https://i.kek.sh/'+c
+    #   print(url_ll)
+    return img_url
 
 
 
@@ -85,14 +99,3 @@ def reciveimg():
 # # Running app
 if __name__ == '__main__':
 	app.run(debug=True, host='localhost', port=5000)
-
-
-
-
-
-
-
-
-
-
-
